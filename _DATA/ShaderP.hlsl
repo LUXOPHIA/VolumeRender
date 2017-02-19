b36f9beaa626eed957f37b7b38a966634c5319f2
luxidea <contact@luxidea.net>
2017-02-20 04:19:46 +0900

    GetVolumeBS4 関数に高速計算手法を導入。

--- _DATA/ShaderP.hlsl
+++ _DATA/ShaderP.hlsl
@@ -26,7 +26,9 @@ inline TRay newTRay( const float3 Pos_, const float3 Vec_ )
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
 
-const SamplerState _Sampler {};
+static const float FLOAT_MAX = 3.402823466E+38;
+
+SamplerState _Sampler {};
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
 
@@ -34,35 +36,51 @@ static int3 _VoxelsN;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
-inline int3 sign3( const float3 P_ )
+inline int3 sign3( const float3 V_ )
 {
     int3 Result;
 
-    Result.x = sign( P_.x );
-    Result.y = sign( P_.y );
-    Result.z = sign( P_.z );
+    Result.x = sign( V_.x );
+    Result.y = sign( V_.y );
+    Result.z = sign( V_.z );
 
     return Result;
 }
 
-inline float3 abs3( const float3 P_ )
+inline float3 abs3( const float3 V_ )
 {
     float3 Result;
 
-    Result.x = abs( P_.x );
-    Result.y = abs( P_.y );
-    Result.z = abs( P_.z );
+    Result.x = abs( V_.x );
+    Result.y = abs( V_.y );
+    Result.z = abs( V_.z );
 
     return Result;
 }
 
-inline int3 floor3( const float3 P_ )
+inline float3 floor3( const float3 V_ )
 {
-    int3 Result;
+    float3 Result;
 
-    Result.x = floor( P_.x );
-    Result.y = floor( P_.y );
-    Result.z = floor( P_.z );
+    Result.x = floor( V_.x );
+    Result.y = floor( V_.y );
+    Result.z = floor( V_.z );
+
+    return Result;
+}
+
+inline float Pow2( const float X_ )
+{
+    return X_ * X_;
+}
+
+inline float3 Pow2( const float3 V_ )
+{
+    float3 Result;
+
+    Result.x = Pow2( V_.x );
+    Result.y = Pow2( V_.y );
+    Result.z = Pow2( V_.z );
 
     return Result;
 }
@@ -88,139 +106,64 @@ inline int MinI( const float3 V_ )
 
 ////////////////////////////////////////////////////////////////////////////////
 
-inline float4 GetVolume( const float3 G_ )
+inline float4 GetVolumeBS1( const float3 G_ )
 {
     return _Texture3D.Sample( _Sampler, ( G_ + 0.5 ) / _VoxelsN );
 }
 
-inline float4 GetVolume( const float X_, const float Y_, const float Z_ )
+inline float4 GetVolumeBS1( const float X_, const float Y_, const float Z_ )
 {
-    return GetVolume( float3( X_, Y_, Z_ ) );
+    return GetVolumeBS1( float3( X_, Y_, Z_ ) );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-inline float BSplin4( const float X_ )
+inline float4 GetVolumeBS4( float3 G_ )
 {
-    float X = abs( X_ );
+    int3 Gi = floor3( G_ );
 
-    if ( X < 1 ) return ( 0.5 * X - 1 ) * X * X + 2.0/3.0;
-            else
-    if ( X < 2 ) return ( ( 1.0 - 1.0/6.0 * X ) * X - 2.0 ) * X + 4.0/3.0;
-            else return 0;
-}
-
-inline float4 BSpline4( const float4 C0_,
-                        const float4 C1_,
-                        const float4 C2_,
-                        const float4 C3_,
-                        const float  Td_ )
-{
-    return C0_ * BSplin4( Td_ + 1 )
-         + C1_ * BSplin4( Td_     )
-         + C2_ * BSplin4( Td_ - 1 )
-         + C3_ * BSplin4( Td_ - 2 );
-}
-
-inline float4 GetVolumeBS4( const float3 G_ )
-{
-    int3   Gi = floor3( G_ );
     float3 Gd = G_ - Gi;
+    float3 Gb = 1  - Gd;
 
-    float4 C000 = GetVolume( Gi.x-1, Gi.y-1, Gi.z-1 );
-    float4 C001 = GetVolume( Gi.x  , Gi.y-1, Gi.z-1 );
-    float4 C002 = GetVolume( Gi.x+1, Gi.y-1, Gi.z-1 );
-    float4 C003 = GetVolume( Gi.x+2, Gi.y-1, Gi.z-1 );
-    float4 C010 = GetVolume( Gi.x-1, Gi.y  , Gi.z-1 );
-    float4 C011 = GetVolume( Gi.x  , Gi.y  , Gi.z-1 );
-    float4 C012 = GetVolume( Gi.x+1, Gi.y  , Gi.z-1 );
-    float4 C013 = GetVolume( Gi.x+2, Gi.y  , Gi.z-1 );
-    float4 C020 = GetVolume( Gi.x-1, Gi.y+1, Gi.z-1 );
-    float4 C021 = GetVolume( Gi.x  , Gi.y+1, Gi.z-1 );
-    float4 C022 = GetVolume( Gi.x+1, Gi.y+1, Gi.z-1 );
-    float4 C023 = GetVolume( Gi.x+2, Gi.y+1, Gi.z-1 );
-    float4 C030 = GetVolume( Gi.x-1, Gi.y+2, Gi.z-1 );
-    float4 C031 = GetVolume( Gi.x  , Gi.y+2, Gi.z-1 );
-    float4 C032 = GetVolume( Gi.x+1, Gi.y+2, Gi.z-1 );
-    float4 C033 = GetVolume( Gi.x+2, Gi.y+2, Gi.z-1 );
+    float3 Gd2 = Pow2( Gd );
+    float3 Gb2 = Pow2( Gb );
 
-    float4 C100 = GetVolume( Gi.x-1, Gi.y-1, Gi.z   );
-    float4 C101 = GetVolume( Gi.x  , Gi.y-1, Gi.z   );
-    float4 C102 = GetVolume( Gi.x+1, Gi.y-1, Gi.z   );
-    float4 C103 = GetVolume( Gi.x+2, Gi.y-1, Gi.z   );
-    float4 C110 = GetVolume( Gi.x-1, Gi.y  , Gi.z   );
-    float4 C111 = GetVolume( Gi.x  , Gi.y  , Gi.z   );
-    float4 C112 = GetVolume( Gi.x+1, Gi.y  , Gi.z   );
-    float4 C113 = GetVolume( Gi.x+2, Gi.y  , Gi.z   );
-    float4 C120 = GetVolume( Gi.x-1, Gi.y+1, Gi.z   );
-    float4 C121 = GetVolume( Gi.x  , Gi.y+1, Gi.z   );
-    float4 C122 = GetVolume( Gi.x+1, Gi.y+1, Gi.z   );
-    float4 C123 = GetVolume( Gi.x+2, Gi.y+1, Gi.z   );
-    float4 C130 = GetVolume( Gi.x-1, Gi.y+2, Gi.z   );
-    float4 C131 = GetVolume( Gi.x  , Gi.y+2, Gi.z   );
-    float4 C132 = GetVolume( Gi.x+1, Gi.y+2, Gi.z   );
-    float4 C133 = GetVolume( Gi.x+2, Gi.y+2, Gi.z   );
+    float3 B0 =           Gb2 *       Gb     / 6;
+    float3 B1 = ( 4 - 3 * Gd2 * ( 2 - Gd ) ) / 6;
+    float3 B2 = ( 4 - 3 * Gb2 * ( 2 - Gb ) ) / 6;
+    float3 B3 =           Gd2 *       Gd     / 6;
 
-    float4 C200 = GetVolume( Gi.x-1, Gi.y-1, Gi.z+1 );
-    float4 C201 = GetVolume( Gi.x  , Gi.y-1, Gi.z+1 );
-    float4 C202 = GetVolume( Gi.x+1, Gi.y-1, Gi.z+1 );
-    float4 C203 = GetVolume( Gi.x+2, Gi.y-1, Gi.z+1 );
-    float4 C210 = GetVolume( Gi.x-1, Gi.y  , Gi.z+1 );
-    float4 C211 = GetVolume( Gi.x  , Gi.y  , Gi.z+1 );
-    float4 C212 = GetVolume( Gi.x+1, Gi.y  , Gi.z+1 );
-    float4 C213 = GetVolume( Gi.x+2, Gi.y  , Gi.z+1 );
-    float4 C220 = GetVolume( Gi.x-1, Gi.y+1, Gi.z+1 );
-    float4 C221 = GetVolume( Gi.x  , Gi.y+1, Gi.z+1 );
-    float4 C222 = GetVolume( Gi.x+1, Gi.y+1, Gi.z+1 );
-    float4 C223 = GetVolume( Gi.x+2, Gi.y+1, Gi.z+1 );
-    float4 C230 = GetVolume( Gi.x-1, Gi.y+2, Gi.z+1 );
-    float4 C231 = GetVolume( Gi.x  , Gi.y+2, Gi.z+1 );
-    float4 C232 = GetVolume( Gi.x+1, Gi.y+2, Gi.z+1 );
-    float4 C233 = GetVolume( Gi.x+2, Gi.y+2, Gi.z+1 );
+    float3 W0 = B0 + B1;
+    float3 W1 = B2 + B3;
 
-    float4 C300 = GetVolume( Gi.x-1, Gi.y-1, Gi.z+2 );
-    float4 C301 = GetVolume( Gi.x  , Gi.y-1, Gi.z+2 );
-    float4 C302 = GetVolume( Gi.x+1, Gi.y-1, Gi.z+2 );
-    float4 C303 = GetVolume( Gi.x+2, Gi.y-1, Gi.z+2 );
-    float4 C310 = GetVolume( Gi.x-1, Gi.y  , Gi.z+2 );
-    float4 C311 = GetVolume( Gi.x  , Gi.y  , Gi.z+2 );
-    float4 C312 = GetVolume( Gi.x+1, Gi.y  , Gi.z+2 );
-    float4 C313 = GetVolume( Gi.x+2, Gi.y  , Gi.z+2 );
-    float4 C320 = GetVolume( Gi.x-1, Gi.y+1, Gi.z+2 );
-    float4 C321 = GetVolume( Gi.x  , Gi.y+1, Gi.z+2 );
-    float4 C322 = GetVolume( Gi.x+1, Gi.y+1, Gi.z+2 );
-    float4 C323 = GetVolume( Gi.x+2, Gi.y+1, Gi.z+2 );
-    float4 C330 = GetVolume( Gi.x-1, Gi.y+2, Gi.z+2 );
-    float4 C331 = GetVolume( Gi.x  , Gi.y+2, Gi.z+2 );
-    float4 C332 = GetVolume( Gi.x+1, Gi.y+2, Gi.z+2 );
-    float4 C333 = GetVolume( Gi.x+2, Gi.y+2, Gi.z+2 );
+    float3 I0 = Gi - 1 + B1 / W0;
+    float3 I1 = Gi + 1 + B3 / W1;
 
-    float4 C00 = BSpline4( C000, C001, C002, C003, Gd.x );
-    float4 C01 = BSpline4( C010, C011, C012, C013, Gd.x );
-    float4 C02 = BSpline4( C020, C021, C022, C023, Gd.x );
-    float4 C03 = BSpline4( C030, C031, C032, C033, Gd.x );
+    //                         BS4(G_)
+    //             W0          |           W1
+    //             +-----------+-----------+
+    //       B0    |     B1          B2    |     B3
+    //  =====#=====$=====#=====*=====#=====$=====#=====
+    //       Gi-1  I0    Gi    G_    Gi+1  I1    Gi+2
 
-    float4 C10 = BSpline4( C100, C101, C102, C103, Gd.x );
-    float4 C11 = BSpline4( C110, C111, C112, C113, Gd.x );
-    float4 C12 = BSpline4( C120, C121, C122, C123, Gd.x );
-    float4 C13 = BSpline4( C130, C131, C132, C133, Gd.x );
+    float4 C000 = GetVolumeBS1( I0.x, I0.y, I0.z );
+    float4 C001 = GetVolumeBS1( I1.x, I0.y, I0.z );
+    float4 C010 = GetVolumeBS1( I0.x, I1.y, I0.z );
+    float4 C011 = GetVolumeBS1( I1.x, I1.y, I0.z );
+    float4 C100 = GetVolumeBS1( I0.x, I0.y, I1.z );
+    float4 C101 = GetVolumeBS1( I1.x, I0.y, I1.z );
+    float4 C110 = GetVolumeBS1( I0.x, I1.y, I1.z );
+    float4 C111 = GetVolumeBS1( I1.x, I1.y, I1.z );
 
-    float4 C20 = BSpline4( C200, C201, C202, C203, Gd.x );
-    float4 C21 = BSpline4( C210, C211, C212, C213, Gd.x );
-    float4 C22 = BSpline4( C220, C221, C222, C223, Gd.x );
-    float4 C23 = BSpline4( C230, C231, C232, C233, Gd.x );
+    float4 C00 = W0.x * C000 + W1.x * C001;
+    float4 C01 = W0.x * C010 + W1.x * C011;
+    float4 C10 = W0.x * C100 + W1.x * C101;
+    float4 C11 = W0.x * C110 + W1.x * C111;
 
-    float4 C30 = BSpline4( C300, C301, C302, C303, Gd.x );
-    float4 C31 = BSpline4( C310, C311, C312, C313, Gd.x );
-    float4 C32 = BSpline4( C320, C321, C322, C323, Gd.x );
-    float4 C33 = BSpline4( C330, C331, C332, C333, Gd.x );
+    float4 C0 = W0.y * C00 + W1.y * C01;
+    float4 C1 = W0.y * C10 + W1.y * C11;
 
-    float4 C0 = BSpline4( C00, C01, C02, C03, Gd.y );
-    float4 C1 = BSpline4( C10, C11, C12, C13, Gd.y );
-    float4 C2 = BSpline4( C20, C21, C22, C23, Gd.y );
-    float4 C3 = BSpline4( C30, C31, C32, C33, Gd.y );
-
-    return BSpline4( C0, C1, C2, C3, Gd.z );
+    return W0.z * C0 + W1.z * C1;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -229,7 +172,7 @@ inline float4 GetField( const float3 P_ )
 {
     float3 G = P_ / _Size * _VoxelsN - 0.5;
 
-    return GetVolume   ( G );  // Linear    Interpolation
+    return GetVolumeBS1( G );  // Linear    Interpolation
   //return GetVolumeBS4( G );  // B-Spline4 Interpolation
 }
 
@@ -242,7 +185,7 @@ inline float4 GetAccum1( const TRay R_, const float T0_, const float T1_ )
     float C = ( T1_ + T0_ ) / 2;
     float R = ( T1_ - T0_ ) / 2;
 
-    float A = Gs[ 0 ].w * GetField( R_.Vec * ( R * Gs[ 0 ].x + C ) + R_.Pos );
+    float4 A = Gs[ 0 ].w * GetField( R_.Vec * ( R * Gs[ 0 ].x + C ) + R_.Pos );
 
     return R * A;
 }
@@ -387,9 +330,15 @@ TResultP MainP( const TSenderP _Sender )
     float3 Gd = G - Gi;
 
     float3 Ts;
-    if ( R.Vec.x > 0 ) Ts.x = Tv.x * ( 1 - Gd.x ); else Ts.x = Tv.x * Gd.x;
-    if ( R.Vec.y > 0 ) Ts.y = Tv.y * ( 1 - Gd.y ); else Ts.y = Tv.y * Gd.y;
-    if ( R.Vec.z > 0 ) Ts.z = Tv.z * ( 1 - Gd.z ); else Ts.z = Tv.z * Gd.z;
+
+    if ( isinf( Tv.x ) ) Ts.x = FLOAT_MAX;
+                    else Ts.x = Tv.x * ( 0.5 + sign( R.Vec.x ) * ( 0.5 - Gd.x ) );
+
+    if ( isinf( Tv.y ) ) Ts.y = FLOAT_MAX;
+                    else Ts.y = Tv.y * ( 0.5 + sign( R.Vec.y ) * ( 0.5 - Gd.y ) );
+
+    if ( isinf( Tv.z ) ) Ts.z = FLOAT_MAX;
+                    else Ts.z = Tv.z * ( 0.5 + sign( R.Vec.z ) * ( 0.5 - Gd.z ) );
 
     Result.Col = 0;
 
