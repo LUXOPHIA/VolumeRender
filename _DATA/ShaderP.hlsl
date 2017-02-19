27c4823c62995d174d317631439a4fcfa9f2a80a
luxidea <contact@luxidea.net>
2017-02-19 09:20:26 +0900

    BSplin4 補間法を追加。

--- _DATA/ShaderP.hlsl
+++ _DATA/ShaderP.hlsl
@@ -1,5 +1,13 @@
 ﻿//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
+struct TGaussPoin
+{
+    float w;
+    float x;
+};
+
+//------------------------------------------------------------------------------
+
 struct TRay
 {
     float3 Pos;
@@ -16,19 +24,13 @@ inline TRay newTRay( const float3 Pos_, const float3 Vec_ )
     return Result;
 }
 
-//------------------------------------------------------------------------------
-
-struct TGaussPoin
-{
-    float w;
-    float x;
-};
-
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
 
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【設定】
+const SamplerState _Sampler {};
 
-SamplerState _SamplerState {};
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+static int3 _VoxelsN;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
@@ -86,14 +88,173 @@ inline int MinI( const float3 V_ )
 
 ////////////////////////////////////////////////////////////////////////////////
 
-inline float4 GetVolume2( const TRay R_, const float T0_, const float T1_ )
+inline float4 GetVolume( const float3 G_ )
+{
+    return _Texture3D.Sample( _Sampler, ( G_ + 0.5 ) / _VoxelsN );
+}
+
+inline float4 GetVolume( const float X_, const float Y_, const float Z_ )
+{
+    return GetVolume( float3( X_, Y_, Z_ ) );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline float BSplin4( const float X_ )
+{
+    float X = abs( X_ );
+
+    if ( X < 1 ) return ( 0.5 * X - 1 ) * X * X + 2.0/3.0;
+            else
+    if ( X < 2 ) return ( ( 1.0 - 1.0/6.0 * X ) * X - 2.0 ) * X + 4.0/3.0;
+            else return 0;
+}
+
+inline float4 BSpline4( const float4 C0_,
+                        const float4 C1_,
+                        const float4 C2_,
+                        const float4 C3_,
+                        const float  Td_ )
+{
+    return C0_ * BSplin4( Td_ + 1 )
+         + C1_ * BSplin4( Td_     )
+         + C2_ * BSplin4( Td_ - 1 )
+         + C3_ * BSplin4( Td_ - 2 );
+}
+
+inline float4 GetVolumeBS4( const float3 G_ )
+{
+    int3   Gi = floor3( G_ );
+    float3 Gd = G_ - Gi;
+
+    float4 C000 = GetVolume( Gi.x-1, Gi.y-1, Gi.z-1 );
+    float4 C001 = GetVolume( Gi.x  , Gi.y-1, Gi.z-1 );
+    float4 C002 = GetVolume( Gi.x+1, Gi.y-1, Gi.z-1 );
+    float4 C003 = GetVolume( Gi.x+2, Gi.y-1, Gi.z-1 );
+    float4 C010 = GetVolume( Gi.x-1, Gi.y  , Gi.z-1 );
+    float4 C011 = GetVolume( Gi.x  , Gi.y  , Gi.z-1 );
+    float4 C012 = GetVolume( Gi.x+1, Gi.y  , Gi.z-1 );
+    float4 C013 = GetVolume( Gi.x+2, Gi.y  , Gi.z-1 );
+    float4 C020 = GetVolume( Gi.x-1, Gi.y+1, Gi.z-1 );
+    float4 C021 = GetVolume( Gi.x  , Gi.y+1, Gi.z-1 );
+    float4 C022 = GetVolume( Gi.x+1, Gi.y+1, Gi.z-1 );
+    float4 C023 = GetVolume( Gi.x+2, Gi.y+1, Gi.z-1 );
+    float4 C030 = GetVolume( Gi.x-1, Gi.y+2, Gi.z-1 );
+    float4 C031 = GetVolume( Gi.x  , Gi.y+2, Gi.z-1 );
+    float4 C032 = GetVolume( Gi.x+1, Gi.y+2, Gi.z-1 );
+    float4 C033 = GetVolume( Gi.x+2, Gi.y+2, Gi.z-1 );
+
+    float4 C100 = GetVolume( Gi.x-1, Gi.y-1, Gi.z   );
+    float4 C101 = GetVolume( Gi.x  , Gi.y-1, Gi.z   );
+    float4 C102 = GetVolume( Gi.x+1, Gi.y-1, Gi.z   );
+    float4 C103 = GetVolume( Gi.x+2, Gi.y-1, Gi.z   );
+    float4 C110 = GetVolume( Gi.x-1, Gi.y  , Gi.z   );
+    float4 C111 = GetVolume( Gi.x  , Gi.y  , Gi.z   );
+    float4 C112 = GetVolume( Gi.x+1, Gi.y  , Gi.z   );
+    float4 C113 = GetVolume( Gi.x+2, Gi.y  , Gi.z   );
+    float4 C120 = GetVolume( Gi.x-1, Gi.y+1, Gi.z   );
+    float4 C121 = GetVolume( Gi.x  , Gi.y+1, Gi.z   );
+    float4 C122 = GetVolume( Gi.x+1, Gi.y+1, Gi.z   );
+    float4 C123 = GetVolume( Gi.x+2, Gi.y+1, Gi.z   );
+    float4 C130 = GetVolume( Gi.x-1, Gi.y+2, Gi.z   );
+    float4 C131 = GetVolume( Gi.x  , Gi.y+2, Gi.z   );
+    float4 C132 = GetVolume( Gi.x+1, Gi.y+2, Gi.z   );
+    float4 C133 = GetVolume( Gi.x+2, Gi.y+2, Gi.z   );
+
+    float4 C200 = GetVolume( Gi.x-1, Gi.y-1, Gi.z+1 );
+    float4 C201 = GetVolume( Gi.x  , Gi.y-1, Gi.z+1 );
+    float4 C202 = GetVolume( Gi.x+1, Gi.y-1, Gi.z+1 );
+    float4 C203 = GetVolume( Gi.x+2, Gi.y-1, Gi.z+1 );
+    float4 C210 = GetVolume( Gi.x-1, Gi.y  , Gi.z+1 );
+    float4 C211 = GetVolume( Gi.x  , Gi.y  , Gi.z+1 );
+    float4 C212 = GetVolume( Gi.x+1, Gi.y  , Gi.z+1 );
+    float4 C213 = GetVolume( Gi.x+2, Gi.y  , Gi.z+1 );
+    float4 C220 = GetVolume( Gi.x-1, Gi.y+1, Gi.z+1 );
+    float4 C221 = GetVolume( Gi.x  , Gi.y+1, Gi.z+1 );
+    float4 C222 = GetVolume( Gi.x+1, Gi.y+1, Gi.z+1 );
+    float4 C223 = GetVolume( Gi.x+2, Gi.y+1, Gi.z+1 );
+    float4 C230 = GetVolume( Gi.x-1, Gi.y+2, Gi.z+1 );
+    float4 C231 = GetVolume( Gi.x  , Gi.y+2, Gi.z+1 );
+    float4 C232 = GetVolume( Gi.x+1, Gi.y+2, Gi.z+1 );
+    float4 C233 = GetVolume( Gi.x+2, Gi.y+2, Gi.z+1 );
+
+    float4 C300 = GetVolume( Gi.x-1, Gi.y-1, Gi.z+2 );
+    float4 C301 = GetVolume( Gi.x  , Gi.y-1, Gi.z+2 );
+    float4 C302 = GetVolume( Gi.x+1, Gi.y-1, Gi.z+2 );
+    float4 C303 = GetVolume( Gi.x+2, Gi.y-1, Gi.z+2 );
+    float4 C310 = GetVolume( Gi.x-1, Gi.y  , Gi.z+2 );
+    float4 C311 = GetVolume( Gi.x  , Gi.y  , Gi.z+2 );
+    float4 C312 = GetVolume( Gi.x+1, Gi.y  , Gi.z+2 );
+    float4 C313 = GetVolume( Gi.x+2, Gi.y  , Gi.z+2 );
+    float4 C320 = GetVolume( Gi.x-1, Gi.y+1, Gi.z+2 );
+    float4 C321 = GetVolume( Gi.x  , Gi.y+1, Gi.z+2 );
+    float4 C322 = GetVolume( Gi.x+1, Gi.y+1, Gi.z+2 );
+    float4 C323 = GetVolume( Gi.x+2, Gi.y+1, Gi.z+2 );
+    float4 C330 = GetVolume( Gi.x-1, Gi.y+2, Gi.z+2 );
+    float4 C331 = GetVolume( Gi.x  , Gi.y+2, Gi.z+2 );
+    float4 C332 = GetVolume( Gi.x+1, Gi.y+2, Gi.z+2 );
+    float4 C333 = GetVolume( Gi.x+2, Gi.y+2, Gi.z+2 );
+
+    float4 C00 = BSpline4( C000, C001, C002, C003, Gd.x );
+    float4 C01 = BSpline4( C010, C011, C012, C013, Gd.x );
+    float4 C02 = BSpline4( C020, C021, C022, C023, Gd.x );
+    float4 C03 = BSpline4( C030, C031, C032, C033, Gd.x );
+
+    float4 C10 = BSpline4( C100, C101, C102, C103, Gd.x );
+    float4 C11 = BSpline4( C110, C111, C112, C113, Gd.x );
+    float4 C12 = BSpline4( C120, C121, C122, C123, Gd.x );
+    float4 C13 = BSpline4( C130, C131, C132, C133, Gd.x );
+
+    float4 C20 = BSpline4( C200, C201, C202, C203, Gd.x );
+    float4 C21 = BSpline4( C210, C211, C212, C213, Gd.x );
+    float4 C22 = BSpline4( C220, C221, C222, C223, Gd.x );
+    float4 C23 = BSpline4( C230, C231, C232, C233, Gd.x );
+
+    float4 C30 = BSpline4( C300, C301, C302, C303, Gd.x );
+    float4 C31 = BSpline4( C310, C311, C312, C313, Gd.x );
+    float4 C32 = BSpline4( C320, C321, C322, C323, Gd.x );
+    float4 C33 = BSpline4( C330, C331, C332, C333, Gd.x );
+
+    float4 C0 = BSpline4( C00, C01, C02, C03, Gd.y );
+    float4 C1 = BSpline4( C10, C11, C12, C13, Gd.y );
+    float4 C2 = BSpline4( C20, C21, C22, C23, Gd.y );
+    float4 C3 = BSpline4( C30, C31, C32, C33, Gd.y );
+
+    return BSpline4( C0, C1, C2, C3, Gd.z );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline float4 GetField( const float3 P_ )
+{
+    float3 G = P_ / _Size * _VoxelsN - 0.5;
+
+    return GetVolume   ( G );  // Linear    Interpolation
+  //return GetVolumeBS4( G );  // B-Spline4 Interpolation
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline float4 GetAccum1( const TRay R_, const float T0_, const float T1_ )
+{
+    const TGaussPoin Gs[ 1 ] = { { 2, 0 } };
+
+    float C = ( T1_ + T0_ ) / 2;
+    float R = ( T1_ - T0_ ) / 2;
+
+    float A = Gs[ 0 ].w * GetField( R_.Vec * ( R * Gs[ 0 ].x + C ) + R_.Pos );
+
+    return R * A;
+}
+
+inline float4 GetAccum2( const TRay R_, const float T0_, const float T1_ )
 {
     const TGaussPoin G[ 1 ] = { { 1.0, sqrt( 1.0 / 3.0 ) } };
 
     const TGaussPoin Gs[ 2 ] = { { G[ 0 ].w, -G[ 0 ].x },
                                  { G[ 0 ].w, +G[ 0 ].x } };
 
-    float4 _Result = 0;
+    float4 Result = 0;
 
     float C = ( T1_ + T0_ ) / 2;
     float R = ( T1_ - T0_ ) / 2;
@@ -101,15 +262,13 @@ inline float4 GetVolume2( const TRay R_, const float T0_, const float T1_ )
     [unroll]
     for ( uint I = 0; I < 2; I++ )
     {
-        float3 P = R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos;
-
-        _Result += Gs[ I ].w * _Texture3D.Sample( _SamplerState, P / _Size );
+        Result += Gs[ I ].w * GetField( R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos );
     }
 
-    return R * _Result;
+    return R * Result;
 }
 
-inline float4 GetVolume3( const TRay R_, const float T0_, const float T1_ )
+inline float4 GetAccum3( const TRay R_, const float T0_, const float T1_ )
 {
     const TGaussPoin G[ 2 ] = { { 8.0 / 9.0, 0.0               },
                                 { 5.0 / 9.0, sqrt( 3.0 / 5.0 ) } };
@@ -118,7 +277,7 @@ inline float4 GetVolume3( const TRay R_, const float T0_, const float T1_ )
                                  { G[ 0 ].w,  G[ 0 ].x },
                                  { G[ 1 ].w, +G[ 1 ].x } };
 
-    float4 _Result = 0;
+    float4 Result = 0;
 
     float C = ( T1_ + T0_ ) / 2;
     float R = ( T1_ - T0_ ) / 2;
@@ -126,15 +285,13 @@ inline float4 GetVolume3( const TRay R_, const float T0_, const float T1_ )
     [unroll]
     for ( uint I = 0; I < 3; I++ )
     {
-        float3 P = R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos;
-
-        _Result += Gs[ I ].w * _Texture3D.Sample( _SamplerState, P / _Size );
+        Result += Gs[ I ].w * GetField( R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos );
     }
 
-    return R * _Result;
+    return R * Result;
 }
 
-inline float4 GetVolume4( const TRay R_, const float T0_, const float T1_ )
+inline float4 GetAccum4( const TRay R_, const float T0_, const float T1_ )
 {
     const TGaussPoin G[ 2 ] = { { ( 18.0 + sqrt( 30.0 ) ) / 36.0, sqrt( ( 3.0 - 2.0 * sqrt( 6.0 / 5.0 ) ) / 7.0 ) },
                                 { ( 18.0 - sqrt( 30.0 ) ) / 36.0, sqrt( ( 3.0 + 2.0 * sqrt( 6.0 / 5.0 ) ) / 7.0 ) } };
@@ -144,7 +301,7 @@ inline float4 GetVolume4( const TRay R_, const float T0_, const float T1_ )
                                  { G[ 0 ].w, +G[ 0 ].x },
                                  { G[ 1 ].w, +G[ 1 ].x } };
 
-    float4 _Result = 0;
+    float4 Result = 0;
 
     float C = ( T1_ + T0_ ) / 2;
     float R = ( T1_ - T0_ ) / 2;
@@ -152,15 +309,13 @@ inline float4 GetVolume4( const TRay R_, const float T0_, const float T1_ )
     [unroll]
     for ( uint I = 0; I < 4; I++ )
     {
-        float3 P = R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos;
-
-        _Result += Gs[ I ].w * _Texture3D.Sample( _SamplerState, P / _Size );
+        Result += Gs[ I ].w * GetField( R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos );
     }
 
-    return R * _Result;
+    return R * Result;
 }
 
-inline float4 GetVolume5( const TRay R_, const float T0_, const float T1_ )
+inline float4 GetAccum5( const TRay R_, const float T0_, const float T1_ )
 {
     const TGaussPoin G[ 3 ] = { {   128.0                         / 225.0, 0.0                                          },
                                 { ( 322.0 + 13.0 * sqrt( 70.0 ) ) / 900.0, sqrt( 5.0 - 2.0 * sqrt( 10.0 / 7.0 ) ) / 3.0 },
@@ -172,7 +327,7 @@ inline float4 GetVolume5( const TRay R_, const float T0_, const float T1_ )
                                  { G[ 1 ].w, +G[ 1 ].x },
                                  { G[ 2 ].w, +G[ 2 ].x } };
 
-    float4 _Result = 0;
+    float4 Result = 0;
 
     float C = ( T1_ + T0_ ) / 2;
     float R = ( T1_ - T0_ ) / 2;
@@ -180,12 +335,10 @@ inline float4 GetVolume5( const TRay R_, const float T0_, const float T1_ )
     [unroll]
     for ( uint I = 0; I < 5; I++ )
     {
-        float3 P = R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos;
-
-        _Result += Gs[ I ].w * _Texture3D.Sample( _SamplerState, P / _Size );
+        Result += Gs[ I ].w * GetField( R_.Vec * ( R * Gs[ I ].x + C ) + R_.Pos );
     }
 
-    return R * _Result;
+    return R * Result;
 }
 
 //##############################################################################
@@ -205,14 +358,13 @@ struct TResultP
 
 TResultP MainP( const TSenderP _Sender )
 {
+    _Texture3D.GetDimensions( _VoxelsN.x, _VoxelsN.y, _VoxelsN.z );
+
     TResultP Result;
 
     float3 E = mul( _EyePos, _MatrixGL ).xyz;
 
     TRay R = newTRay( _Sender.Pos.xyz, normalize( _Sender.Pos.xyz - E ) );
-
-    int3 _VoxelsN;
-    _Texture3D.GetDimensions( _VoxelsN.x, _VoxelsN.y, _VoxelsN.z );
 
     int3 Gv = sign3( R.Vec );
 
@@ -228,7 +380,7 @@ TResultP MainP( const TSenderP _Sender )
                         {    0, Tv.y,    0 },
                         {    0,    0, Tv.z } };
 
-    float3 G = R.Pos / Sd - float3( 0.5, 0.5, 0.5 );
+    float3 G = R.Pos / Sd - 0.5;
 
     int3 Gi = floor3( G );
 
@@ -252,7 +404,7 @@ TResultP MainP( const TSenderP _Sender )
 
         float T1 = Ts[ K ];
 
-        Result.Col += GetVolume2( R, T0, T1 );
+        Result.Col += GetAccum2( R, T0, T1 );
 
         T0 = T1;
 
